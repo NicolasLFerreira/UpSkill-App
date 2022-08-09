0f2793abb9722d77b2a17c51b3f47146ba509a1c
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-08-09 13:22:41 +1200

    Added cors

--- Server/UpSkill API/Program.cs
+++ Server/UpSkill API/Program.cs
@@ -3,6 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Cors configuration
+string corsPolicy = "Policy1";
+builder.Services.AddCors(config =>
+{
+    config.AddPolicy(corsPolicy,
+        policy =>
+    {
+        policy.WithOrigins("http://localhost:3000");
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
