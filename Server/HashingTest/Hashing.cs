ca777d6bc05fd691d74b07ad7f88432a9362ebc6
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-08-21 14:18:29 +1200

    App for testing the hash stuff

---
+++ Server/HashingTest/Hashing.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace HashingTest
+{
+    public class Hashing
+    {
+        public string?[] Hasher(string password)
+        {
+            Random random = new Random();
+            byte[] salt = new byte[32];
+
+            for (int i = 0; i < salt.Length; i++)
+            {
+                salt[i] = (byte)random.Next();
+            }
+
+            return new string[2] { BitConverter.ToString(KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA512, 512, 32)).Replace("-", ""), BitConverter.ToString(salt).Replace("-", "") };
+        }
+    }
+}
---
+++ Server/HashingTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using HashingTest;
+
+Hashing h = new();
+
+string?[] s = h.Hasher("test");
+
+Console.WriteLine(s[0]);
+Console.WriteLine(s[1]);
