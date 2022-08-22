8483761259e152847897279e307c4d164272a44c
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-08-22 23:23:15 +1200

    cleaned HelpPage.tsx from the example testing

--- Client/upskill-client/src/Components/HelpPage.tsx
+++ Client/upskill-client/src/Components/HelpPage.tsx
@@ -1,17 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import React, { Fragment } from "react";
 
 export default function HelpScreen() {
     return (
         <Fragment>
-            <Box>
+            <Container>
                 <Typography>
                     This is the help page of the website. Most of your questions will be answered here.
                 </Typography>
                 <Typography>
                     click buttons for stuff to happen
                 </Typography>
-            </Box>
+            </Container>
         </Fragment>
     );
 }
