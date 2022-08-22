bebcf2b94230e3618bb71c6ccfb007516ab29b98
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-08-22 23:23:32 +1200

    cleaned HelpPage.tsx from the example testing ACTUAL

--- Client/upskill-client/src/Components/HelpPage.tsx
+++ Client/upskill-client/src/Components/HelpPage.tsx
@@ -4,14 +4,7 @@ import React, { Fragment } from "react";
 export default function HelpScreen() {
     return (
         <Fragment>
-            <Container>
-                <Typography>
-                    This is the help page of the website. Most of your questions will be answered here.
-                </Typography>
-                <Typography>
-                    click buttons for stuff to happen
-                </Typography>
-            </Container>
+
         </Fragment>
     );
 }
