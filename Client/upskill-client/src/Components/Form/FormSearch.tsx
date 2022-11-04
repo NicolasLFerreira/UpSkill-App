3a181442a4297ca21ab3671cad56060a43d8a893
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-11-05 12:52:14 +1300

    Removed leftover code and improved code readability

--- Client/upskill-client/src/Components/Form/FormSearch.tsx
+++ Client/upskill-client/src/Components/Form/FormSearch.tsx
@@ -1,12 +1,9 @@
-import React, { ChangeEvent, Component, ReactNode } from "react";
-import { List, Button, TextField } from "@mui/material";
+import React, { Component, ReactNode } from "react";
+import { List, Button } from "@mui/material";
 import { default as Box } from "@mui/material/Unstable_Grid2/Grid2";
 import IStudent from "../../types/IStudent";
 import StudentDataCrud from "../../services/StudentDataCrud";
-import studentSearch from "../../studentSearch";
 import { blueGrey } from "@mui/material/colors";
-import SelectMultiple from "../SelectMultiple";
-import { studentProperties } from "../../utility/StudentUtility";
 import StudentFilter from "../StudentFilter";
 
 interface IProps {
@@ -14,12 +11,8 @@ interface IProps {
 }
 interface IState {
     students: Array<IStudent>,
-    studentsFiltered: Array<IStudent>,
-    propertiesFiltered: Array<string>,
-    searchString: string
+    studentsFiltered: Array<IStudent>
 }
-
-const defaultFilteredProperties: Array<string> = ["firstName", "lastName"];
 
 export default class FormSearch extends Component<IProps, IState>{
     constructor(props: IProps) {
@@ -27,19 +20,17 @@ export default class FormSearch extends Component<IProps, IState>{
 
         this.state = {
             students: [],
-            studentsFiltered: [],
-            propertiesFiltered: defaultFilteredProperties,
-            searchString: ""
+            studentsFiltered: []
         }
     }
 
     componentDidMount(): void {
-        this.retrieveStudents();
+        this.getStudents();
     }
 
     // Get student data
 
-    retrieveStudents = () => {
+    getStudents = () => {
         StudentDataCrud.getAll()
             .then((response: any) => {
                 this.setState({
@@ -57,7 +48,15 @@ export default class FormSearch extends Component<IProps, IState>{
     StudentContainer(student: IStudent) {
         return (
             <Box sx={{ m: 1 }}>
-                <Button variant="contained" sx={{ width: "100%", backgroundColor: blueGrey[700] }} onClick={() => this.props.callback(student)}>
+                <Button
+                    variant="contained"
+                    sx={{
+                        width: "100%",
+                        backgroundColor: blueGrey[700]
+                    }}
+                    onClick={() =>
+                        this.props.callback(student)
+                    }>
                     ({student.yearLevel}) {student.lastName}, {student.firstName}
                 </Button>
             </Box>
@@ -79,7 +78,12 @@ export default class FormSearch extends Component<IProps, IState>{
         return (
             <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1, minHeight: 0 }}>
                 <StudentFilter
-                    callback={(students: Array<IStudent>) => this.registerChange(students)} mode={false} />
+                    callback={
+                        (students: Array<IStudent>) =>
+                            this.registerChange(students)
+                    }
+                    mode={false}
+                />
                 <List style={{ width: "100%", overflow: "auto", flexGrow: 1, minHeight: 0 }}>
                     {array}
                 </List>
