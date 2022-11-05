e91aab88e1bf1e5a67027d14c82e038bf8617e50
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-11-06 10:55:22 +1300

    Removed unused stuff

--- Client/upskill-client/src/index.tsx
+++ Client/upskill-client/src/index.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import App from './Components/App';
-import DatabaseScreen from './Components/Datagrid/DatabasePage';
-import HelpScreen from './Components/Help/HelpPage';
-import HomeScreen from './Components/Home/HomePage';
-import FormPage from './Components/Form/FormPage';
-import NavigationBar from './Components/NavigationBar';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import App from "./Components/App";
+import DatabaseScreen from "./Components/Datagrid/DatabasePage";
+import HelpScreen from "./Components/Help/HelpPage";
+import HomeScreen from "./Components/Home/HomePage";
+import FormPage from "./Components/Form/FormPage";
+import NavigationBar from "./Components/NavigationBar";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<NavigationBar />
-			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<Navigate to="home" />} />
-					<Route path="home" element={<HomeScreen />} />
-					<Route path="database" element={<DatabaseScreen />} />
-					<Route path="form" element={<FormPage />} />
-					<Route path="form/:studentId" element={<FormPage />} />
-					<Route path="help" element={<HelpScreen />} />
-					<Route path="*" element={<Navigate to="/home" />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <NavigationBar />
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<Navigate to="home" />} />
+                    <Route path="home" element={<HomeScreen />} />
+                    <Route path="database" element={<DatabaseScreen />} />
+                    <Route path="form" element={<FormPage />} />
+                    <Route path="form/:studentId" element={<FormPage />} />
+                    <Route path="help" element={<HelpScreen />} />
+                    <Route path="*" element={<Navigate to="/home" />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
