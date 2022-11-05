7f1546edc1e9010d2d26597a78e76269b7d6b710
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-11-06 12:20:50 +1300

    Updated from class component to functional component

--- Client/upskill-client/src/Components/Form/FormSearch.tsx
+++ Client/upskill-client/src/Components/Form/FormSearch.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { List, Button, darken } from "@mui/material";
 import { default as Box } from "@mui/material/Unstable_Grid2/Grid2";
 import IStudent from "../../types/IStudent";
@@ -7,32 +7,23 @@ import { blueGrey } from "@mui/material/colors";
 import StudentFilter from "../StudentFilter";
 import { Link } from "react-router-dom";
 
-interface IProps {}
 interface IState {
 	students: Array<IStudent>;
 	studentsFiltered: Array<IStudent>;
 }
 
-export default class FormSearch extends Component<IProps, IState> {
-	constructor(props: IProps) {
-		super(props);
+const defaultState: IState = {
+	students: [],
+	studentsFiltered: [],
+};
 
-		this.state = {
-			students: [],
-			studentsFiltered: [],
-		};
-	}
+function FormSearchF() {
+	const [state, setState] = useState<IState>(defaultState);
 
-	componentDidMount(): void {
-		this.getStudents();
-	}
-
-	// Get student data
-
-	getStudents = () => {
+	const getStudents = () => {
 		StudentDataCrud.getAll()
 			.then((response: any) => {
-				this.setState({
+				setState({
 					students: response.data,
 					studentsFiltered: response.data,
 				});
@@ -43,7 +34,18 @@ export default class FormSearch extends Component<IProps, IState> {
 			});
 	};
 
-	StudentContainer(student: IStudent) {
+	useEffect(() => {
+		getStudents();
+	}, []);
+
+	const registerChange = (students: Array<IStudent>) => {
+		setState((previous) => ({
+			...previous,
+			studentsFiltered: students,
+		}));
+	};
+
+	const StudentContainer = (student: IStudent) => {
 		return (
 			<Box sx={{ m: 1 }}>
 				<Button
@@ -60,61 +62,135 @@ export default class FormSearch extends Component<IProps, IState> {
 				>
 					{`(${student.yearLevel}) ${student.lastName}, ${student.firstName}`}
 				</Button>
-				{/* <Button
-                    sx={{
-                        width: "100%",
-                        backgroundColor: blueGrey[700],
-                        "&:hover": {
-                            backgroundColor: darken(blueGrey[700], 0.8),
-                        },
-                    }}
-                    variant="contained"
-                    onClick={() => this.props.callback(student)}
-                >
-                    {`(${student.yearLevel}) ${student.lastName}, ${student.firstName}`}
-                </Button> */}
 			</Box>
 		);
-	}
-
-	registerChange = (students: Array<IStudent>) => {
-		this.setState({
-			studentsFiltered: students,
-		});
 	};
 
-	render() {
-		var array: Array<ReactNode> = [];
-		this.state.studentsFiltered.forEach((student) => {
-			array.push(this.StudentContainer(student));
-		});
+	var studentArray: Array<ReactNode> = [];
+	state.studentsFiltered.forEach((student) => {
+		studentArray.push(StudentContainer(student));
+	});
 
-		return (
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "column",
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				flexGrow: 1,
+				minHeight: 0,
+			}}
+		>
+			<StudentFilter
+				callback={(students: Array<IStudent>) =>
+					registerChange(students)
+				}
+				mode={false}
+			/>
+			<List
+				style={{
+					width: "100%",
+					overflow: "auto",
 					flexGrow: 1,
 					minHeight: 0,
 				}}
 			>
-				<StudentFilter
-					callback={(students: Array<IStudent>) =>
-						this.registerChange(students)
-					}
-					mode={false}
-				/>
-				<List
-					style={{
-						width: "100%",
-						overflow: "auto",
-						flexGrow: 1,
-						minHeight: 0,
-					}}
-				>
-					{array}
-				</List>
-			</Box>
-		);
-	}
+				{studentArray}
+			</List>
+		</Box>
+	);
 }
+
+export default FormSearchF;
+
+// class FormSearchC extends Component<IProps, IState> {
+// 	constructor(props: IProps) {
+// 		super(props);
+
+// 		this.state = {
+// 			students: [],
+// 			studentsFiltered: [],
+// 		};
+// 	}
+
+// 	componentDidMount(): void {
+// 		this.getStudents();
+// 	}
+
+// 	// Get student data
+
+// 	getStudents = () => {
+// 		StudentDataCrud.getAll()
+// 			.then((response: any) => {
+// 				this.setState({
+// 					students: response.data,
+// 					studentsFiltered: response.data,
+// 				});
+// 				console.log(response.data);
+// 			})
+// 			.catch((e: Error) => {
+// 				console.log(e);
+// 			});
+// 	};
+
+// 	StudentContainer(student: IStudent) {
+// 		return (
+// 			<Box sx={{ m: 1 }}>
+// 				<Button
+// 					variant="contained"
+// 					component={Link}
+// 					to={`/form/${student.studentId}`}
+// 					sx={{
+// 						width: "100%",
+// 						backgroundColor: blueGrey[700],
+// 						"&:hover": {
+// 							backgroundColor: darken(blueGrey[700], 0.8),
+// 						},
+// 					}}
+// 				>
+// 					{`(${student.yearLevel}) ${student.lastName}, ${student.firstName}`}
+// 				</Button>
+// 			</Box>
+// 		);
+// 	}
+
+// 	registerChange = (students: Array<IStudent>) => {
+// 		this.setState({
+// 			studentsFiltered: students,
+// 		});
+// 	};
+
+// 	render() {
+// 		var array: Array<ReactNode> = [];
+// 		this.state.studentsFiltered.forEach((student) => {
+// 			array.push(this.StudentContainer(student));
+// 		});
+
+// 		return (
+// 			<Box
+// 				sx={{
+// 					display: "flex",
+// 					flexDirection: "column",
+// 					flexGrow: 1,
+// 					minHeight: 0,
+// 				}}
+// 			>
+// 				<StudentFilter
+// 					callback={(students: Array<IStudent>) =>
+// 						this.registerChange(students)
+// 					}
+// 					mode={false}
+// 				/>
+// 				<List
+// 					style={{
+// 						width: "100%",
+// 						overflow: "auto",
+// 						flexGrow: 1,
+// 						minHeight: 0,
+// 					}}
+// 				>
+// 					{array}
+// 				</List>
+// 			</Box>
+// 		);
+// 	}
+// }
