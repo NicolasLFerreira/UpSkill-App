bc384c9bda6e8f2bc3632d702b16111a80b9a2db
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2022-11-07 13:37:52 +1300

    Renamed

---
+++ Client/upskill-client/src/AxiosInstance.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+var url: string = "https://localhost:7156/api";
+
+export default axios.create({
+	baseURL: url,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
